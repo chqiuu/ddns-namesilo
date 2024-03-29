90e9246493bf5ffe146e46ef1c81e404dabd8ec5
chqiuu <chqiuu@qq.com>
2024-03-30 07:17:42 +0800

    代码优化

--- src/main/java/com/chqiuu/ddnsnamesilo/task/DdnsTask.java
+++ src/main/java/com/chqiuu/ddnsnamesilo/task/DdnsTask.java
@@ -28,37 +28,54 @@ public class DdnsTask {
      * <p>
      * https://www.namesilo.com/account/api-manager
      * <p>
-     * 执行频率，每次启动项目后固定频率执行 30分钟
+     * 执行频率，每次启动项目后固定频率执行 5分钟
      */
-    @Scheduled(initialDelay = 5000, fixedDelay = 30 * 60 * 1000)
+    @Scheduled(initialDelay = 5000, fixedDelay = 300 * 1000)
     public void ddnsTask() {
-        String[] ipUrls = new String[]{"https://ip.3322.net", "https://4.ipw.cn", "https://myip.ipip.net", "https://ddns.oray.com/checkip"};
-        String localIp = null;
-        for (String url : ipUrls) {
-            try {
-                String res = restTemplate.getForObject(url, String.class);
-                if (res != null) {
-                    // log.info("{} return {}", url, res);
-                    if (res.trim().startsWith("Current IP Address: ")) {
-                        localIp = res.substring(res.indexOf("Current IP Address: ") + "Current IP Address: ".length()).trim();
-                    } else if (res.trim().startsWith("当前 IP：")) {
-                        localIp = res.substring(res.indexOf("当前 IP：") + "当前 IP：".length(), res.indexOf("  ")).trim();
-                    } else {
-                        localIp = res.trim();
-                    }
-                    log.info("{} localIp [{}] {}", url, localIp, Ipv4Util.ipv4ToLong(localIp));
-                }
-            } catch (Exception e) {
-                log.error("{} 获取IP失败！", url);
-            }
-            if (StrUtil.isNotBlank(localIp) && Ipv4Util.ipv4ToLong(localIp, -1L) < 0) {
-                break;
-            }
-        }
         if (properties.getDomains() == null || properties.getDomains().length == 0) {
             log.error("请配置您的域名");
             return;
         }
+        String localIp = getLocalIp();
+        if (StrUtil.isBlank(localIp)) {
+            log.error("获取本机IP失败");
+            return;
+        }
+        if (StrUtil.isBlank(Constant.Public.DOMAIN_IP_ADDRESS)) {
+            getListRecords();
+        }
+        log.info("当前域名映射IP：{}", Constant.Public.DOMAIN_IP_ADDRESS);
+        if (Constant.Public.DOMAIN_IP_ADDRESS == null) {
+            log.error("当前域名映射IP为空，请检查namesilo是否配置正确！");
+            return;
+        }
+        if (localIp.equals(Constant.Public.DOMAIN_IP_ADDRESS)) {
+            log.info("IP一致，不需要更新映射IP localIp:{},domainIP:{}", localIp, Constant.Public.DOMAIN_IP_ADDRESS);
+        } else {
+            getListRecords();
+            log.info("IP不一致，需要更新映射IP localIp:{},domainIP:{}", localIp, Constant.Public.DOMAIN_IP_ADDRESS);
+            dnsUpdateRecord(localIp);
+        }
+    }
+
+    private void dnsUpdateRecord(String localIp) {
+        // 修改域名映射IP
+        try {
+            String mainDomain = getMainDomain(properties.getDomains()[0]);
+            Constant.Public.DOMAIN_MAP.forEach((recordId, domain) -> {
+                String url = String.format("https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=%s&domain=%s&rrid=%s&rrhost=%s&rrvalue=%s&rrttl=3601", properties.getNamesiloApiKey(), mainDomain, recordId, domain, localIp);
+                log.info("设置url {}", url);
+                String res = restTemplate.getForObject(url, String.class);
+                if (res != null) {
+                    log.info("设置完成！{}", res);
+                }
+            });
+        } catch (Exception e) {
+            log.error("namesilo连接失败1！");
+        }
+    }
+
+    private void getListRecords() {
         String mainDomain = getMainDomain(properties.getDomains()[0]);
         Constant.Public.DOMAIN_MAP = new HashMap<>();
         try {
@@ -85,30 +102,33 @@ public class DdnsTask {
         } catch (Exception e) {
             log.error("namesilo连接失败！");
         }
-        log.info("当前域名映射IP：{}", Constant.Public.DOMAIN_IP_ADDRESS);
-        if (Constant.Public.DOMAIN_IP_ADDRESS == null) {
-            log.error("当前域名映射IP为空，请检查namesilo是否配置正确！");
-            return;
-        }
-        if (localIp.equals(Constant.Public.DOMAIN_IP_ADDRESS)) {
-            log.info("IP一致，不需要更新映射IP localIp:{},domainIP:{}", localIp, Constant.Public.DOMAIN_IP_ADDRESS);
-        } else {
-            log.info("IP不一致，需要更新映射IP localIp:{},domainIP:{}", localIp, Constant.Public.DOMAIN_IP_ADDRESS);
-            // 修改域名映射IP
+    }
+
+    private String getLocalIp() {
+        String localIp = null;
+        String[] ipUrls = new String[]{"https://ip.3322.net", "https://4.ipw.cn", "https://myip.ipip.net", "https://ddns.oray.com/checkip"};
+        for (String url : ipUrls) {
             try {
-                String finalLocalIp = localIp;
-                Constant.Public.DOMAIN_MAP.forEach((recordId, domain) -> {
-                    String url = String.format("https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=%s&domain=%s&rrid=%s&rrhost=%s&rrvalue=%s&rrttl=7207", properties.getNamesiloApiKey(), mainDomain, recordId, domain, finalLocalIp);
-                    log.info("设置url {}", url);
-                    String res = restTemplate.getForObject(url, String.class);
-                    if (res != null) {
-                        log.info("设置完成！{}", res);
+                String res = restTemplate.getForObject(url, String.class);
+                if (res != null) {
+                    // log.info("{} return {}", url, res);
+                    if (res.trim().startsWith("Current IP Address: ")) {
+                        localIp = res.substring(res.indexOf("Current IP Address: ") + "Current IP Address: ".length()).trim();
+                    } else if (res.trim().startsWith("当前 IP：")) {
+                        localIp = res.substring(res.indexOf("当前 IP：") + "当前 IP：".length(), res.indexOf("  ")).trim();
+                    } else {
+                        localIp = res.trim();
                     }
-                });
+                    log.info("{} localIp [{}] {}", url, localIp, Ipv4Util.ipv4ToLong(localIp));
+                }
             } catch (Exception e) {
-                log.error("namesilo连接失败1！");
+                log.error("{} 获取IP失败！", url);
+            }
+            if (StrUtil.isNotBlank(localIp) && Ipv4Util.ipv4ToLong(localIp, -1L) > 0) {
+                break;
             }
         }
+        return localIp;
     }
 
     private String getMainDomain(String domain) {
